d75d4dc8bbb090d8989adf36b6a75d66ea77ecc4
munna1127 <maibatatahu1127@gmail.com>
2026-08-12 21:37:05 +0530

    Added Safalta Quiz branding, Decimal Marking Scheme, and Admin Test Timer

--- models/Question.js
+++ models/Question.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const questionSchema = new mongoose.Schema({
     examName: { type: String, required: true },
     testName: { type: String, required: true },
-    subject: { type: String, required: true, default: "General" }, // NAYA FEATURE: Subject
-    topic: { type: String, required: true, default: "Mixed" },     // NAYA FEATURE: Chapter/Topic
+    subject: { type: String, required: true, default: "General" },
+    topic: { type: String, required: true, default: "Mixed" },
     text: { type: String, required: true },
     options: [{ type: String, required: true }],
     correctOptionIndex: { type: Number, required: true },
-    positiveMarks: { type: Number, default: 4 },
-    negativeMarks: { type: Number, default: 1 }
+    positiveMarks: { type: Number, default: 4 }, // Ab ye decimals support karega
+    negativeMarks: { type: Number, default: 1 }, // Ab ye decimals support karega
+    testDuration: { type: Number, default: 60 }  // NAYA: Test Timer (in Minutes)
 });
 
 module.exports = mongoose.model('Question', questionSchema);
