b5ad4ec0efc8dc22704742edac0ed3e84cea92e8
munna1127 <maibatatahu1127@gmail.com>
2026-08-12 20:51:51 +0530

    Phase 1: Added Subject and Topic tracking to Admin Database

--- models/Question.js
+++ models/Question.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const questionSchema = new mongoose.Schema({
     examName: { type: String, required: true },
     testName: { type: String, required: true },
+    subject: { type: String, required: true, default: "General" }, // NAYA FEATURE: Subject
+    topic: { type: String, required: true, default: "Mixed" },     // NAYA FEATURE: Chapter/Topic
     text: { type: String, required: true },
     options: [{ type: String, required: true }],
     correctOptionIndex: { type: Number, required: true },
